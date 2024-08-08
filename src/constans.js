8e73cd3f69e296c505c9cf47b619070919b2df08
Daniel Amado <daniel.amadove@gmail.com>
2024-08-08 10:24:13 -0500

    cambios en el Hero, imagen diferente al de la seccion quien es sandy

--- src/constans.js
+++ src/constans.js
@@ -1,3 +1,3 @@
-export const URL_BASE = "/hm/cursos/despierta_claves_del_marketing_antes_de_iniciar_tu_propio_negocio/";
+export const URL_BASE = "/l/despierta_claves_del_marketing_antes_de_iniciar_tu_propio_negocio/";
 export const CheckoutProducto = "?add-to-cart=600";
 export const URL_WEB_CHECKOUT = "https://cursos.market-support.com/checkout/";
