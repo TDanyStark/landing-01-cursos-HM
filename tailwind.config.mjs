f98575645e5ee9965e9e726c704e2f61c81b0eb8
Daniel Amado <daniel.amadove@gmail.com>
2024-10-21 17:38:28 -0500

    Refactorizar componente Subtituloh3 y agregar nuevo componente Titulo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
 				'morado-market': '#692394',
 				'rosado-market': '#c21f8c', 
 				'verde-market': '#00ef04',
-				'verde-success': '#28a745',
+				'verde-success': '#198754',
 				'gray-fondo-card': '#ffffff',
 				'lila-fondo': '#eeecf7',
 				'gray-texto': '#7d7d7cff',
